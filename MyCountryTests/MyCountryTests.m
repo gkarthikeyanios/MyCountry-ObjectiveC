8df175748d3ccff79c6caa000ef2f5f49190d100
karthikeyan <igkarthi@yahoo.com>
2018-02-15 04:23:40 +0530

    Initial Commit

---
+++ MyCountryTests/MyCountryTests.m
@@ -0,0 +1,39 @@
+//
+//  MyCountryTests.m
+//  MyCountryTests
+//
+//  Created by Lexicon on 15/02/18.
+//  Copyright © 2018 Lexicon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyCountryTests : XCTestCase
+
+@end
+
+@implementation MyCountryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
